6457d7038d094755f677162b105cb7bcb136d612
Administrator <Administrator@PC-12>
2025-12-06 18:55:40 +0330

    Add project files.

---
+++ DotnetFandamental0409.LearningConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetFandamental0409.LearningConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World2!");
+        }
+    }
+}
